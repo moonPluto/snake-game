9210a1906905f7ceff9a68f59c46a5d3a288e217
moonPluto <129724505+moonPluto@users.noreply.github.com>
2023-09-11 15:40:58 +0530

    Update snaky.py

--- snaky.py
+++ snaky.py
@@ -20,6 +20,12 @@ CYAN = (0,255,255)
 WHITE = (255, 255, 255)
 PINK = (255,0,255)
 YELLOW = (255,255,0)
+CHOCO=(210,105,30)
+LAVARIS=(230,230,250)
+RBLUE=(65,105,225)
+SGREEN=(0,255,127)
+VIOLET=(138,43,226)
+LILVER=(192,192,192)
 BLACK = (0,0,0)
 
 SIZE = 20
@@ -94,21 +100,37 @@ class SnakeGame:
     def collision(self):
         if self.head in self.snake[1:]:
             return True
-        
         return False
         
     def update(self):
         self.display.fill(BLACK)
         
         for pt in self.snake:
-            if self.snake.index(pt)%4==0:
-                pygame.draw.rect(self.display, WHITE, pygame.Rect(pt.x, pt.y, SIZE, SIZE),3)
-            elif self.snake.index(pt)%3==0:
-                pygame.draw.ellipse(self.display, YELLOW, pygame.Rect(pt.x, pt.y, SIZE, SIZE))
-            elif self.snake.index(pt)%2==0:
-                pygame.draw.rect(self.display, CYAN, pygame.Rect(pt.x, pt.y, SIZE, SIZE))
+            if pt==self.head:
+                pygame.draw.line(self.display, WHITE, (pt.x,pt.y),(pt.x+SIZE,pt.y+SIZE),2)
+                pygame.draw.line(self.display, WHITE, (pt.x,pt.y+SIZE),(pt.x+SIZE,pt.y),2)
+                pygame.draw.line(self.display, WHITE, (pt.x,pt.y+(SIZE//2)),(pt.x+SIZE,pt.y+(SIZE//2)),2)
+                pygame.draw.line(self.display, WHITE, (pt.x+(SIZE//2),pt.y),(pt.x+(SIZE//2),pt.y+SIZE),2)
+            elif self.snake.index(pt)%10==1:
+                pygame.draw.ellipse(self.display, PINK, pygame.Rect(pt.x, pt.y, SIZE, SIZE),5)
+            elif self.snake.index(pt)%10==2:
+                pygame.draw.rect(self.display, CYAN, pygame.Rect(pt.x, pt.y, SIZE, SIZE),1)
+            elif self.snake.index(pt)%10==3:
+                pygame.draw.ellipse(self.display, YELLOW, pygame.Rect(pt.x, pt.y, SIZE, SIZE),5)
+            elif self.snake.index(pt)%10==4:
+                pygame.draw.rect(self.display, CHOCO, pygame.Rect(pt.x, pt.y, SIZE, SIZE),1)
+            elif self.snake.index(pt)%10==5:
+                pygame.draw.ellipse(self.display, LAVARIS, pygame.Rect(pt.x, pt.y, SIZE, SIZE),5)
+            elif self.snake.index(pt)%10==6:
+                pygame.draw.rect(self.display, RBLUE, pygame.Rect(pt.x, pt.y, SIZE, SIZE),1)
+            elif self.snake.index(pt)%10==7:
+                pygame.draw.ellipse(self.display, SGREEN, pygame.Rect(pt.x, pt.y, SIZE, SIZE),5)
+            elif self.snake.index(pt)%10==8:
+                pygame.draw.rect(self.display, VIOLET, pygame.Rect(pt.x, pt.y, SIZE, SIZE),1)
+            elif self.snake.index(pt)%10==9:
+                pygame.draw.ellipse(self.display, LILVER, pygame.Rect(pt.x, pt.y, SIZE, SIZE),5)
             else:
-                pygame.draw.ellipse(self.display, PINK, pygame.Rect(pt.x, pt.y, SIZE, SIZE),3)
+                pygame.draw.rect(self.display, WHITE, pygame.Rect(pt.x, pt.y, SIZE, SIZE),1)
 
         pygame.draw.ellipse(self.display, RED, pygame.Rect(self.food.x, self.food.y, SIZE, SIZE))
         
